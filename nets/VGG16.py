f7be78f7b7cece75845fcc3af09f77ad205d15ee
Daniel Rodriguez <dani1610@hotmail.com>
2021-09-01 15:31:39 +0100

    Added file with tries of the VGG16

---
+++ nets/VGG16.py
@@ -0,0 +1,15 @@
+from torchvision.models import vgg16
+
+vgg16_model = vgg16()
+
+# for child in vgg16_model.children():
+#     print(child)
+
+object_methods = [method_name for method_name in dir(vgg16_model)
+                  if callable(getattr(vgg16_model, method_name))]
+
+print(object_methods)
+
+
+# vgg16_model.classifier = vgg16_model.classifier[:-1]
+# print(vgg16_model.classifier)
