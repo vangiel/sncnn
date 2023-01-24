191957e0474a14097f9a1c91cba418bd4c6ad786
vangiel <dani1610@hotmail.com>
2023-01-24 21:04:07 +0000

    Adjustments to calculate the MSE in the test set

--- calculate_mse.py
+++ calculate_mse.py
@@ -1,6 +1,7 @@
 import sys
 
 import numpy as np
+import cv2
 from PIL import Image
 from pathlib import Path
 
@@ -10,13 +11,17 @@ if len(sys.argv) != 2:
 
 mse_accumulate = []
 for file_idx, fake_path in enumerate(Path(sys.argv[1]).glob('*fake*')):
-    file_name = str(fake_path).split('/')[-1].split('_')[0]
+    names = str(fake_path).split('/')[-1].split('_')[0:-2]
+    file_name = ''
+    for n in names:
+        file_name = file_name + n + '_'
+    file_name = file_name[:-1]
 
     true_path = ''
     list_path = str(fake_path).split('/')
     for idx in range(len(list_path) - 1):
         true_path += ('/' + list_path[idx])
-    true_path += ('/' + file_name + '_real_B.png')
+    true_path += ('/' + file_name + '__Q1.png')
     true_path = true_path[1:]
 
     if file_idx % 10 == 0:
@@ -24,6 +29,13 @@ for file_idx, fake_path in enumerate(Path(sys.argv[1]).glob('*fake*')):
 
     image_fake = np.array(Image.open(str(fake_path))).astype('float')/255
     image_gt = np.array(Image.open(true_path)).astype('float')/255
+
+    image_gt = cv2.resize(image_gt, dsize=(256, 256), interpolation=cv2.INTER_CUBIC)
+
+    image_fake = image_fake[:, :, 0]
+    if len(image_gt.shape) == 3:
+        image_gt = image_gt[:, :, 0]
+
 
     mse = ((image_gt - image_fake) ** 2).mean(axis=0)
     mse_accumulate.append(mse)
